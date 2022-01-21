0930e5eac2835f2f11e253448f48e53d54a539ed
Aaron Winter <aaron@winterware.com>
2022-01-21 15:38:09 -0800

    added list for moving multiple selected units
    thanks Boyuan for getting the NavMeshAgents set up it made a lot more sense to me with those in place :)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,41 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System;
 
 public class Movement : MonoBehaviour
 {
 
-    public NavMeshAgent playerAgent;
-    public Animator animator;
+    public List<GameObject> selected;
 
     // Start is called before the first frame update
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0)) //Left click
         {
+            //Toggle selection
             RaycastHit hitPos;
-
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitPos, Mathf.Infinity))
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitPos, Mathf.Infinity, 1 << 7)) //Layermask 7 (Unit) only
             {
-                playerAgent.SetDestination(hitPos.point);
+                GameObject unitHit = hitPos.transform.gameObject;
 
-                animator.SetFloat("Speed_f", 0.3f);
-                animator.SetBool("Static_b", false);
+                if (!selected.Remove(unitHit)) //And check that its actually controllable by the player
+                {
+                    selected.Add(unitHit);
+                }
             }
         }
 
-        if (playerAgent.remainingDistance == 0)
+        if (Input.GetMouseButtonDown(1)) //Right click
         {
-            animator.SetFloat("Speed_f", 0f);
-            animator.SetBool("Static_b", true);
-        }
-    }
+            RaycastHit hitPos;
+            bool hitEnemy = false;
 
-    private void OnMouseDown()
-    {
-        Debug.Log("Clicked character");
+            //Check first if clicked a unit (to attack it)
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitPos, Mathf.Infinity, 1 << 7)) //Layermask 7 (Unit) only
+            {
+                //Implement logic for attacking here
+                //  Likely in the form of ~if hitPos.transform.gameObject == ENEMY -> attack
+                GameObject unitHit = hitPos.transform.gameObject;
+                hitEnemy = !selected.Contains(unitHit); 
+                    //!Array.Exists(selected, element => element == unitHit); //for arrays
+
+                if (hitEnemy)
+                {
+                    //Attack
+                    Debug.Log("Attack enemy " + unitHit.name);
+
+                }
+            }
+
+            //Check for where player clicked (if didn't click enemy)
+            if (!hitEnemy && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitPos, Mathf.Infinity, 1<<6)) //Layermask 6 (Ground) only
+            {
+                foreach (GameObject unit in selected)
+                {
+                    NavMeshAgent playerAgent = unit.GetComponent<NavMeshAgent>();
+                    Animator animator = unit.GetComponent<Animator>();
+                    //We can likely do this within the unit at some point, akin to unit.moveTo ... and it will just run this
+                    //  which allows for variable speed, own handling of animations, .....
+
+                    playerAgent.SetDestination(hitPos.point);
+
+                    animator.SetFloat("Speed_f", 0.3f);
+                    animator.SetBool("Static_b", false);
+
+                    if (playerAgent.remainingDistance == 0)
+                    { //Note that this causes the animation to turn to static standing when
+                        //moving after arriving at destination
+                        animator.SetFloat("Speed_f", 0f);
+                        animator.SetBool("Static_b", true);
+                    }
+                }
+            }
+        }
+
     }
 
 
