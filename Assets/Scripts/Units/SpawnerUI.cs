8eabb87b0c5a053ff8033fc27306fd67f5bbf635
Alec-Kaxos <57384294+Alec-Kaxos@users.noreply.github.com>
2022-02-20 22:28:33 -0800

    Updated Spawner Buttons
    The spawner buttons now have a max of 3 resources.  So, units should also have a maximum of 3 resources required to make.  Gave some of the units test values to make sure the buttons worked correctly.

--- Assets/Scripts/Units/SpawnerUI.cs
+++ Assets/Scripts/Units/SpawnerUI.cs
@@ -12,9 +12,15 @@ using System;
 
 public class SpawnerUI : MonoBehaviour
 {
-    public GameObject buttonPrefab;
-    public GameObject content;
-    public GameObject spawner;
+
+    [SerializeField] private GameObject buttonPrefab;
+    [SerializeField] private GameObject content;
+    [SerializeField] private GameObject spawner;
+    [SerializeField] private Sprite FoodIcon;
+    [SerializeField] private Sprite StoneIcon;
+    [SerializeField] private Sprite WoodIcon;
+    [SerializeField] private Sprite GoldIcon;
+    [SerializeField] private Sprite SilverIcon;
 
     // Start is called before the first frame update
     void Start()
@@ -25,13 +31,44 @@ public class SpawnerUI : MonoBehaviour
         {
             GameObject button = Instantiate(buttonPrefab);
             button.transform.SetParent(content.transform);
+            int resource = 1;
 
-            button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = unitTypes[i].name;
-            button.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = "" + unitTypes[i].getCost("Food");
-            button.transform.GetChild(4).GetComponent<TextMeshProUGUI>().text = "" + unitTypes[i].getCost("Stone");
-            button.transform.GetChild(6).GetComponent<TextMeshProUGUI>().text = "" + unitTypes[i].getCost("Wood");
-            button.transform.GetChild(8).GetComponent<TextMeshProUGUI>().text = "" + unitTypes[i].getCost("Gold");
-            button.transform.GetChild(10).GetComponent<TextMeshProUGUI>().text = "" + unitTypes[i].getCost("Silver");
+            button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "" + unitTypes[i].name;
+
+            if(unitTypes[i].getCost("Food")!=0)
+            {
+                button.transform.GetChild(resource).GetComponent<Image>().sprite = FoodIcon;
+                button.transform.GetChild(resource+1).GetComponent<TextMeshProUGUI>().text = "" + unitTypes[i].getCost("Food");
+                resource += 2;
+            }
+
+            if (unitTypes[i].getCost("Stone") != 0)
+            {
+                button.transform.GetChild(resource).GetComponent<Image>().sprite = StoneIcon;
+                button.transform.GetChild(resource + 1).GetComponent<TextMeshProUGUI>().text = "" + unitTypes[i].getCost("Stone");
+                resource += 2;
+            }
+
+            if (unitTypes[i].getCost("Wood") != 0)
+            {
+                button.transform.GetChild(resource).GetComponent<Image>().sprite = WoodIcon;
+                button.transform.GetChild(resource + 1).GetComponent<TextMeshProUGUI>().text = "" + unitTypes[i].getCost("Wood");
+                resource += 2;
+            }
+
+            if (unitTypes[i].getCost("Gold") != 0 && resource<7)
+            {
+                button.transform.GetChild(resource).GetComponent<Image>().sprite = GoldIcon;
+                button.transform.GetChild(resource + 1).GetComponent<TextMeshProUGUI>().text = "" + unitTypes[i].getCost("Gold");
+                resource += 2;
+            }
+
+            if (unitTypes[i].getCost("Gold") != 0 && resource < 7)
+            {
+                button.transform.GetChild(resource).GetComponent<Image>().sprite = SilverIcon;
+                button.transform.GetChild(resource + 1).GetComponent<TextMeshProUGUI>().text = "" + unitTypes[i].getCost("Silver");
+                resource += 2;
+            }
 
             uint localI = i;
             button.GetComponent<Button>().onClick.AddListener(() =>
